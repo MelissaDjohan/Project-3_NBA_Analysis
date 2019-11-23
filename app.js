ab60b7e2d9d5c264fd101a14d95fe1469033bec8
Luan Hajnaj <lxh338@case.edu>
2019-11-23 13:20:08 -0500

    updated Plotly Chart

--- app.js
+++ app.js
@@ -19,27 +19,65 @@
 
 
 
- 
+
 // }
 
-console.log(data)
-var ages = data.map(d=>d.age)
-console.log(ages)
-var salary = data.map(d=>d.salary_2019to2020)
-var height = data.map(d=>d.height)
-console.log(height)
-var trace1 = {
-    x: height,
-    y: salary,
+// console.log(data)
+var ages = data.map(d => d.age)
+var names = data.map(d => d.player)
+var teams = data.map(d => d.tm)
+// console.log(ages)
+var salary = data.map(d => d.salary_2019to2020)
+var height = data.map(d => d.height)
+// console.log(height)
 
+var trace1 = [{
+        x: height,
+        y: salary,
 
-    type: "scatter",
-    
-        // text: text, 
-        mode:`markers`,
-        marker: {size: ages} 
-}
-var layout = {}
+        // type: "scatter",
+        text: names,
+        mode: "markers",
+        marker: {
+                size: salary / 1000000,
+                // sizeref: .0000001,
+                // sizemode: 'area',
+                color: salary
+        },
+        hovertemplate: '<i>Player:</i> %{text}' +
+                '<br><b>Height</b>: %{x:.2f} feet<br>' +
+                '<b>Salary</b>: $%{y:,}',
+        hoverinfo: "hello",
+        name: ""
+}]
+var layout = {
+        title: 'Salary v. Height',
+        showlegend: false,
+        height: 600,
+        width: 800,
+        hovermode: 'closest',
+        xaxis: {
+                title: {
+                        text: 'Age',
+                        font: {
+                                family: 'Courier New, monospace',
+                                size: 18,
+                                color: '#7f7f7f'
+                        }
 
-var bubbleChart =  [trace1];
-Plotly.plot('chart here', bubbleChart, layout);
+                }
+        },
+        yaxis: {
+                title: {
+                        text: 'Salary',
+                        font: {
+                                family: 'Courier New, monospace',
+                                size: 18,
+                                color: '#7f7f7f'
+                        }
+
+                }
+        }
+};
+
+Plotly.plot('chart here', trace1, layout);
